369ada37e4f3df8ce50812e25dded99588533c7a
DohweeKim <cloudkim2024@gmail.com>
2021-04-04 19:14:15 -0400

    exclude , include, block added to base.html

--- minterest/accountapp/views.py
+++ minterest/accountapp/views.py
@@ -4,4 +4,4 @@ from django.http.response import HttpResponse
 # Create your views here.
 def hello_world(request):
     # 왜 바로 안 읽어지냐면 , root app 의 setting.py 에서 추가해줘야한다
-    return render(request, 'base.html')
+    return render(request, 'accountapp/hello_world.html')
